07ff2d9ad3cab0bf4847e8e0d07f049bbf3f117f
VenelinDimitrov <65946212+VenelinDimitrov@users.noreply.github.com>
2021-11-20 20:34:49 +0100

    Creation of the project
    Created my project. Trying out different color schemes and layouts to see what would look good. Created the index page for experimenting with a sample navigation and product list.

---
+++ beardCultureFinalProject/beardCulture/src/main/java/com/example/beardculture/BeardCultureApplication.java
@@ -0,0 +1,13 @@
+package com.example.beardculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeardCultureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeardCultureApplication.class, args);
+    }
+
+}
