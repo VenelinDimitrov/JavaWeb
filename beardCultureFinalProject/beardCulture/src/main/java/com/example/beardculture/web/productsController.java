2ab5afa59d41b6c656e487d9c2f862eab1433076
VenelinDimitrov <65946212+VenelinDimitrov@users.noreply.github.com>
2021-11-21 12:40:44 +0100

    Added pages
    Added the index page, as well as the pages for the oils and the balms. Added the add to subscription for the users and the edit product button for the admins. Still haven't implemented any business logic, so the buttons are visible regardless who opens the page.

---
+++ beardCultureFinalProject/beardCulture/src/main/java/com/example/beardculture/web/productsController.java
@@ -0,0 +1,20 @@
+package com.example.beardculture.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class productsController {
+
+    @GetMapping("/oils")
+    public String oilsPage(){
+        return "oils";
+    }
+
+    @GetMapping("/balms")
+    public String balmsPage(){
+        return "balms";
+    }
+}
