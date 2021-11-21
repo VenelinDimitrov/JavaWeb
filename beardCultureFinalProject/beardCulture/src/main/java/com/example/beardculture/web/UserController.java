f432f7b7a6da7dde7c69efbcaca86eef908828b8
VenelinDimitrov <65946212+VenelinDimitrov@users.noreply.github.com>
2021-11-21 15:34:08 +0100

    Register page
    Added Register page

--- beardCultureFinalProject/beardCulture/src/main/java/com/example/beardculture/web/UserController.java
+++ beardCultureFinalProject/beardCulture/src/main/java/com/example/beardculture/web/UserController.java
@@ -12,4 +12,9 @@ public class UserController {
     public String login(){
         return "login";
     }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
 }
